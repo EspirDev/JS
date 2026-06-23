3d185e943af2e18174409aa4d047b2429c611d44
Evgeniy <EvgeniySpirov@yandex.ru>
2026-06-23 15:03:46 +0300

    fix-1

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -2,5 +2,5 @@
  const age = 18;
  const isDrunk = false;
 
- output = hasLicence && age >= 18  && !isDrunk ? 'Водитель может ехать' : 'Водитель не может ехать';
+ let output = hasLicence && age >= 18  && !isDrunk ? 'Водитель может ехать' : 'Водитель не может ехать';
  console.log(output);
