139bcd5e2077f7cb01b2f9fd0f71cd8e4817c0d5
Evgeniy <EvgeniySpirov@yandex.ru>
2026-06-23 14:56:36 +0300

    Задачка про водителя.

---
+++ 6-driver/app.js
@@ -0,0 +1,6 @@
+ const hasLicence = true;
+ const age = 18;
+ const isDrunk = false;
+
+ output = hasLicence && age >= 18  && !isDrunk ? 'Водитель может ехать' : 'Водитель не может ехать';
+ console.log(output);
