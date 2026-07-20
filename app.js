e5e59dc4217947da8449e71b8a64a96dcfd890c2
Evgeniy <EvgeniySpirov@yandex.ru>
2026-07-20 19:14:43 +0300

    added sort

---
+++ app.js
@@ -0,0 +1,20 @@
+function sortLoops(arr) {
+    let n = arr.length;
+  
+    for (let i = 0; i < n - 1; i++) {
+     
+        for (let j = 0; j < n - 1 - i; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+let arr = [1, 40, -5, 10, 0];
+console.log(sortLoops(arr)); 
